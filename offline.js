a4a0b2522c4cc73eaa150016799602ebb06d5faa
laurachen0729 <110528175+laurachen0729@users.noreply.github.com>
2023-04-06 14:19:42 +0800

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1680761526,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/gopher-sheet0.png",
+		"images/gopher-sheet1.png",
+		"images/hole-sheet0.png",
+		"images/hammer-sheet0.png",
+		"images/star-sheet0.png",
+		"media/mixkit-sci-fi-game-over-1951.ogg",
+		"media/mixkit-player-losing-or-failing-2042.ogg",
+		"media/mixkit-sci-fi-game-over-1951.m4a",
+		"media/mixkit-player-losing-or-failing-2042.m4a",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
